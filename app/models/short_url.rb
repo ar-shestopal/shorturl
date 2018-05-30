8757de39148e0b05951aee89312f3c0cee5e3e3b
ar.shestopal <ar.shestopal@gmail.com>
2018-05-30 15:00:09 +0300

    Added validations specs for ShortUrl

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,3 @@
 class ShortUrl < ApplicationRecord
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,14 @@ def app
   Rails.application
 end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
